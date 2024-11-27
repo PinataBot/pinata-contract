e4eae087acdbe6596d3dc9599cb1b9d27fd61399
hokage229 <ernestzulpukarov@gmail.com>
2024-11-28 01:08:08 +0700

    chore: Remove package.json and associated prettier script

--- prettier.sh
+++
@@ -1,7 +0,0 @@
-# USAGE: ./prettier.sh <path-to-move-file-or-directory>
-
-PATH_TO_MOVE_FILE=$1
-
-echo "##### Prettify Move file #####"
-
-./node_modules/.bin/prettier --plugin=prettier-plugin-move --write $PATH_TO_MOVE_FILE
